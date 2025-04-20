67e2468be364bb8b899bed19e4901df29c99eeab
sparshsharma81 <81arvindarora@gmail.com>
2025-04-20 16:58:36 +0530

    new update 1.0

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({ ///this will take an object..
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",  //this will create an object
+    baseURL: import.meta.env.MODE === "development"
+     ? "http://localhost:5001/api" 
+      : "https://chat-mmln.onrender.com/api",  //ye basically render pr call karega
+     // //this will create an object
     //ye vo backend ka url hai.. jisme ham use karege...
 
     //we are making it get ready for the production mode..
