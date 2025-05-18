9c4dc283c7d48f07837552ee2ab97a39ec0af7b7
sparsh <130464884+sparshsharma81@users.noreply.github.com>
2025-05-18 15:54:45 +0530

    Update Navbar.jsx
    removed horscope butotn code

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -116,9 +116,8 @@ const Navbar = () => {
 
 
 {/*                 {/* Ye hai horoscope ka button */}
-{/*                 <button onClick={handleHoroscopeClick} className="btn btn-sm gap-2"> */}
-{/*                   🔮 <span className="hidden sm:inline">Horoscope</span> */}
-                // </button> */}
+{/* horscoe button ko remove kar diya .... */}
+{/*  */}
 {/* NOW IN THE NEW UPDATE---- I HAVE REMOVED THIS HORSCOPE BUTTON..BECUASE THIS SERVICE IS NO LONGER FREE...MAYBE IN SOME OTHER UPDATE --- TRY TO INTRODUCE IT AGAIN...  */}
                 <button onClick={() => window.open('https://chess-0hn9.onrender.com', '_blank')} className="btn btn-sm gap-2">
    <span className="hidden sm:inline">♕Chess</span>
